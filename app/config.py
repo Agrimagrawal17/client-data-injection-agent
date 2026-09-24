1d4e9b5bbb4c04578533805287599192a45023eb
Agrimagrawal17 <agrimagrawal5623>
2026-09-24 12:03:33 +0530

    Add Docker setup and environment based database config

--- app/config.py
+++ app/config.py
@@ -1,1 +1,6 @@
-DATABASE_URL = "mysql+pymysql://root:root@localhost:3306/client_data_db"
+import os
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "mysql+pymysql://root:root@localhost:3306/client_data_db"
+)
